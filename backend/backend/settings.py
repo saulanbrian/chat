49cbed71240bb402ae0851e56dabdf560b2539e1
saulanbrian <saulanbrian@gmail.com>
2024-07-16 12:23:05 +0800

    add the choreo backend url to allowed host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["chatapp-backend-2308158041-choreo-internal.dp-development-chat-46562-3075123834"]
 
 
 # Application definition
