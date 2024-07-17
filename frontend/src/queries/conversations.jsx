f941421350fda92952bea423fa3e5c4aa79b6a11
saulanbrian <saulanbrian@gmail.com>
2024-07-17 16:52:10 +0800

    add trailing slash to conversation path that probably causing the error

--- frontend/src/queries/conversations.jsx
+++ frontend/src/queries/conversations.jsx
@@ -6,7 +6,7 @@ export const useGetConversations = () => {
   return useQuery({
     queryKey:['conversations'],
     queryFn:async() => {
-      const res = await api.get('conversations')
+      const res = await api.get('conversations/')
       return res.data
     },
   })
